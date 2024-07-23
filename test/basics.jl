bc5ad5f320936fe1c46a74c4ad3c761cc625474b
Alberto Mengali <disberd@gmail.com>
2024-07-23 09:51:15 +0200

    reorganize and add some tests

---
+++ test/basics.jl
@@ -0,0 +1,22 @@
+@testitem "Basics" begin
+    using SlottedRandomAccess
+    using SlottedRandomAccess: replicas_positions
+    using Test
+
+    scheme = MF_CRDSA{3}()
+
+    @test_throws "a multiple of the number of time slots" replicas_positions(scheme, 100)
+
+    test_vec = map(_ -> replicas_positions(scheme, 99), 1:10)
+    @test all(test_vec) do (s1, s2, s3)
+        s1 <= 33 &&
+        33 < s2 <= 66 &&
+        66 < s3 <= 99
+    end
+
+    scheme = MF_CRDSA{2}(3, () -> (1, rand(2:3)))
+    test_vec = map((x) -> scheme.time_slots_function(), 1:10)
+    @test all(x -> x[1] == 1, test_vec)
+    second_time_slots = unique(map(x -> x[2], test_vec))
+    @test sort(second_time_slots) == [2,3]
+end
---
+++ test/readme_example.jl
@@ -0,0 +1,64 @@
+@testitem "Reduced Readme Example" begin
+    using SlottedRandomAccess
+    using PlotlyBase # This is not part of the package env
+    # Generic parameters
+    common = (; M=4, coderate=1 / 3, power_strategy=SamePower)
+    coding_gain = common.coderate * log2(common.M) |> x -> -10log10(x)
+    line_colors = [ # Use to match the line colors
+        "rgb(93, 146, 191)", # 6dB, blue
+        "rgb(233, 71, 72)", # 9dB, red
+        "rgb(113, 191, 109)", # 12dB, green
+    ]
+    ebno_max_vec = [6, 9, 12]
+    # Define the load vector
+    load = 0.1:0.1:.2
+    # Create the CRDSA curves
+    crdsa_sims = map(1:3) do idx
+        ebno_max = ebno_max_vec[idx]
+        line_color = line_colors[idx]
+        # Define the RA scheme to use
+        scheme = CRDSA{3}()
+        # Define the power distribution for the replicas
+        power_dist = LogUniform_dB(2 - coding_gain, ebno_max - coding_gain)
+        # Create the simulation object
+        sim = PLR_Simulation(load;
+            common...,
+            power_dist,
+            scheme,
+            nslots=100,
+        )
+        add_scatter_kwargs!(sim;
+            name="N<sub>rep</sub> = 3, [E<sub>b</sub>N<sub>0</sub>]<sub>max</sub> = $(ebno_max)dB",
+            line_color,
+            line_dash=:solid,
+            marker_symbol=:square,
+        )
+        simulate!(sim) # Compute the packet loss ratio
+    end
+    # Create the MF-CRDSA curves
+    mf_crdsa_sims = map(1:3) do idx
+        ebno_max = ebno_max_vec[idx]
+        line_color = line_colors[idx]
+        # Define the RA scheme to use
+        scheme = MF_CRDSA{3}()
+        # Define the power distribution for the replicas
+        power_dist = LogUniform_dB(2 - coding_gain, ebno_max - coding_gain)
+        # Create the simulation object
+        sim = PLR_Simulation(load;
+            common...,
+            power_dist,
+            scheme,
+            nslots=99,
+        )
+        # Add information to customize the plot to the simulation object
+        add_scatter_kwargs!(sim;
+            name="MF, N<sub>rep</sub> = 3, [E<sub>b</sub>N<sub>0</sub>]<sub>max</sub> = $(ebno_max)dB",
+            line_color,
+            line_dash=:dash,
+            marker_symbol=:diamond
+        )
+        simulate!(sim) # Compute the packet loss ratio
+    end
+    # Plot the MF-CRDSA and CRDSA curves together
+    Plot(vcat(crdsa_sims, mf_crdsa_sims))
+end
